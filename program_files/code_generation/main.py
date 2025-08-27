bb434bbce8c0247268cb87efe2136ed5b0b80749
Jarod Houston <jarhoust@cisco.com>
2025-08-27 13:54:56 -0700

    Auto code generation now creates new attributes

--- program_files/code_generation/main.py
+++ program_files/code_generation/main.py
@@ -1,10 +1,12 @@
+import sys
+import json
 from ruamel.yaml import YAML
 from pprint import pprint
 from program_files.utils.schema_loader import get_schema_path
 from program_files.utils.logger import get_logger, log_error_red
 from program_files.entities.definitions import ENTITY_KEYS, ENTITY_PATHS
-from program_files.code_generation.helpers import camel_to_screaming_snake
-from program_files.code_generation.file_editors import add_entity_to_definitions_file, insert_into_json_schema, register_attributes, \
+from program_files.code_generation.helpers import camel_to_screaming_snake, find_key_path, get_nested
+from program_files.code_generation.file_editors import add_entity_to_definitions_file, insert_into_json_schema, insert_new_attribute, register_attributes, \
     generate_api_function_calls, generate_function_object, insert_entity_processing, add_code_to_fetch
 
 yaml = YAML()
@@ -26,9 +28,12 @@ main_pipeline = "program_files/scripts/handle_json_input.py"
 hyperfabric_api = "program_files/scripts/hyperfabric_api.py"
 get_fabric_config = "program_files/get_fabric_config.py"
 
-# ----------------- SCHEMA FILES -----------------
+# ----------------- SCHEMA FILE -----------------
 validation_json = get_schema_path()
-validation_template = "program_files/schemas/validation/validation_template.yaml"
+
+# ----------------- TEMPLATES -----------------
+infra_template = "program_files/user_templates/infra_template.yaml"
+tenant_template = "program_files/user_templates/tenant_template.yaml"
 
 def validate_new_object(key, obj):
     if not isinstance(obj, list):
@@ -54,6 +59,38 @@ def validate_new_object(key, obj):
     logger.info(f"Object '{key}' is valid")
     return True
 
+def get_new_attributes(schema_data, new_keys):
+    def get_current_attributes(key):
+        path = find_key_path(json_schema, key)
+        target = get_nested(json_schema, path)
+        if "items" not in target or "properties" not in target.get("items"):
+            log_error_red(logger, f"Error trying to fetch current properties of {key} in {json_schema}")
+            return None
+        
+        curr_attributes = []
+        for attr in target["items"]["properties"]:
+            curr_attributes.append(attr)
+        return curr_attributes
+
+    with open(validation_json, "r") as f:
+        json_schema = json.load(f)
+    
+    if not json_schema:
+        log_error_red(logger, f"Error reading in json schema: {json_schema}")
+        sys.exit(1)
+    
+    result = {}
+    for key, obj in schema_data.items():
+        if key in new_keys:
+            continue
+        schema_attributes = get_current_attributes(key)
+        declared_attributes = [attr for attr in obj[0] if attr != "owner"]
+        new_attributes = [attr for attr in declared_attributes if attr not in schema_attributes]
+        if len(new_attributes) > 0:
+            result[key] = new_attributes # Instead of just the name, do a map of name: type
+
+    return result
+
 def get_new_keys(schema_data):
     current_keys = set(ENTITY_KEYS)
     new_keys = []
@@ -70,7 +107,14 @@ def main():
     schema_data = read_in_schema()
     # Before getting just the new keys, we should check the existing objects for new attributes
     new_keys = get_new_keys(schema_data)
+    new_attributes = get_new_attributes(schema_data, new_keys)
+
+    # Handles creating new attributes
+    for key, obj in new_attributes.items():
+        # Modifies schemas/validator.json
+        insert_new_attribute(validation_json, key, obj)
     
+    # Handles creating new objects
     for key in new_keys:
         new_obj = schema_data[key]
         
